9f873442f1d2af6cfa88f689312c4e54723f6def
Clearlove7 <98686153+alison16smotthie@users.noreply.github.com>
2023-10-12 01:33:06 +0700

    edit code

--- src/controller/botFbController.js
+++ src/controller/botFbController.js
@@ -20,174 +20,177 @@ class botFbController {
         return res.send('bot page');
     }
 
-    handlePostback = (sender_psid, received_postback) =>{
-        let response;
-        
-        let payload = received_postback.payload;
-    
-        if (payload === 'yes') {
-          response = { "text": "Cảm ơn!" }
-        } 
-        else if (payload === 'no') {
-          response = { "text": "ohhh, cảm ơn vì bạn đã nhận xét <: " }
-        }
-        
-        this.callSendAPI(sender_psid, response);
-    }
-
-    handleMessage = (sender_psid, received_message) =>{
-
-      let response;        
-      if (received_message.text) {    
-          response = {
-            "text": `${received_message.text}`,
-          }
-      }
-
-      if(received_message.text==="#Tường"){
-        response = {"text": `${received_message.text} : Tường nick name Clearlove7 Profile : `+SearchBot[36],
-        }
-      }
-      else if (received_message.attachments) {
-        let attachment_url = received_message.attachments[0].payload.url;
-        response = {
-          "attachment": {
-            "type": "template",
-            "payload": {
-              "template_type": "generic",
-              "elements": [{
-                "title": "oh ảnh của bạn thật đẹp",
-                "subtitle": "bạn có thấy nó đẹp không?",
-                "image_url": attachment_url,
-                "title":"cảm ơn vì đã gửi ảnh cho tôi ^^",
-                "buttons": [
-                  {
-                    "type": "postback",
-                    "title": "Có rất đẹp ((: !",
-                    "payload": "yes",
-                  },
-                  {
-                    "type": "postback",
-                    "title": "Không đẹp chút nào !",
-                    "payload": "no",
-                  }
-                ],
-              }]
-            }
-          }
-        }
-      } 
-
-      this.callSendAPI(sender_psid, response);   
-
-    }
-
-    postWebhook = (req,res) =>{
-
-        let body = req.body;
-    
-        if (body.object === 'page') {
-    
-            body.entry.forEach(function(entry) 
-        {
-    
-        
-        let webhook_event = entry.messaging[0];
-    
-        console.log('Event : ',webhook_event);
-    
-    
-        let sender_psid = webhook_event.sender.id;
-    
-        console.log('Sender PSID : ' + sender_psid);
-    
-        
-        if (webhook_event.message) {
-    
-            this.handleMessage(sender_psid, webhook_event.message);
-        } 
-    
-        else if (webhook_event.postback) {
-    
-                this.handlePostback(sender_psid, webhook_event.postback);
-    
-            };
-        });
-    
-        res.status(200).send('EVENT_RECEIVED');
-    
-        }
-        
-        else{
-    
-          res.sendStatus(404);
-        }
-    }
-
-    getWebhook = (req,res) =>{
-
-      console.log('verify token : ',VERIFY_TOKEN)
   
-      let mode = req.query["hub.mode"];
-      let token = req.query["hub.verify_token"];
-      let challenge = req.query["hub.challenge"];
-  
-      if(mode && token){
-  
-          if(mode === "subscribe"&&token === VERIFY_TOKEN){ 
-  
-              console.log("WEBHOOK_VERIFY");
-  
-              res.status(200).send(challenge);
-  
-          }else{
-  
-                  res.sendStatus(403);
-              }   
-        }
-    }
-
-    callSendAPI(sender_psid, response) {
-
-        let request_body = {
-    
-          "recipient": {
-    
-            "id": sender_psid
-          },
-    
-          "message": response
-        }
-        
-        request({
-    
-          "uri": "https://graph.facebook.com/v2.6/me/messages",
-          "qs": { "access_token": PAGE_ACCESS_TOKEN },
-          "method": "POST",
-          "json": request_body
-    
-        },(err, res, body) => {
-    
-            if (!err) {
-    
-              console.log('message sent!');//
-            
-            } else {
-    
-              console.error("Unable to send message:" + err);
-            }
-        });
-    }
-
-    
 }
 
 
+let handlePostback = (sender_psid, received_postback) =>{
+    let response;
+    
+    let payload = received_postback.payload;
+
+    if (payload === 'yes') {
+      response = { "text": "Cảm ơn!" }
+    } 
+    else if (payload === 'no') {
+      response = { "text": "ohhh, cảm ơn vì bạn đã nhận xét <: " }
+    }
+    
+    callSendAPI(sender_psid, response);
+}
+
+let handleMessage = (sender_psid, received_message) =>{
+
+  let response;        
+  if (received_message.text) {    
+      response = {
+        "text": `${received_message.text}`,
+      }
+  }
+
+  if(received_message.text==="#Tường"){
+      response = {"text": `${received_message.text} : Tường nick name Clearlove7 Profile : `+SearchBot[36],
+      }
+      console.log(response);
+  }
+  else if (received_message.attachments) {
+    let attachment_url = received_message.attachments[0].payload.url;
+    response = {
+      "attachment": {
+        "type": "template",
+        "payload": {
+          "template_type": "generic",
+          "elements": [{
+            "title": "oh ảnh của bạn thật đẹp",
+            "subtitle": "bạn có thấy nó đẹp không?",
+            "image_url": attachment_url,
+            "title":"cảm ơn vì đã gửi ảnh cho tôi ^^",
+            "buttons": [
+              {
+                "type": "postback",
+                "title": "Có rất đẹp ((: !",
+                "payload": "yes",
+              },
+              {
+                "type": "postback",
+                "title": "Không đẹp chút nào !",
+                "payload": "no",
+              }
+            ],
+          }]
+        }
+      }
+    }
+  } 
+
+  callSendAPI(sender_psid, response);   
+
+}
+
+let postWebhook = (req,res) =>{
+
+  let body = req.body;
+
+  if (body.object === 'page') {
+
+      body.entry.forEach(function(entry) 
+  {
+
+  
+  let webhook_event = entry.messaging[0];
+
+  console.log('Event : ',webhook_event);
+
+
+  let sender_psid = webhook_event.sender.id;
+
+  console.log('Sender PSID : ' + sender_psid);
+
+  
+  if (webhook_event.message) {
+
+     handleMessage(sender_psid, webhook_event.message);
+  } 
+
+  else if (webhook_event.postback) {
+
+          handlePostback(sender_psid, webhook_event.postback);
+
+      };
+  });
+
+  res.status(200).send('EVENT_RECEIVED');
+
+  }
+  
+  else{
+
+    res.sendStatus(404);
+  }
+}
+
+let getWebhook = (req,res) =>{
+
+  console.log('verify token : ',VERIFY_TOKEN)
+
+  let mode = req.query["hub.mode"];
+  let token = req.query["hub.verify_token"];
+  let challenge = req.query["hub.challenge"];
+
+  if(mode && token){
+
+      if(mode === "subscribe"&&token === VERIFY_TOKEN){ 
+
+          console.log("WEBHOOK_VERIFY");
+
+          res.status(200).send(challenge);
+
+      }else{
+
+              res.sendStatus(403);
+          }   
+    }
+}
+
+  
+
+let callSendAPI = (sender_psid, response)=> {
+
+    let request_body = {
+
+      "recipient": {
+
+        "id": sender_psid
+      },
+
+      "message": response
+    }
+    
+    request({
+
+      "uri": "https://graph.facebook.com/v2.6/me/messages",
+      "qs": { "access_token": PAGE_ACCESS_TOKEN },
+      "method": "POST",
+      "json": request_body
+
+    },(err, res, body) => {
+
+        if (!err) {
+
+          console.log('message sent!');//
+        
+        } else {
+
+          console.error("Unable to send message:" + err);
+        }
+    });
+}
 
 
 module.exports = {
     bot_facebook : new botFbController,
-   
+    postWebhook,
+    getWebhook
 };
 
 
--- src/router/web.js
+++ src/router/web.js
@@ -3,13 +3,14 @@ let router = express.Router();
 const {ChatRoute} = require('./chat');
 const {HomeRoute} = require('./home');
 const {bot_facebook} = require('../controller/botFbController');
+const {postWebhook, getWebhook} = require('../controller/botFbController');
 
 
 let webInit = (app) => {
 
     app.get('/botfb', bot_facebook.index);
-    app.get('/webhook', bot_facebook.getWebhook)
-    router.post('/webhook', bot_facebook.postWebhook);
+    app.get('/webhook', getWebhook)
+    router.post('/webhook', postWebhook);
     app.use('/chat', ChatRoute.Router());
     app.use('/', HomeRoute.Router());
 }
