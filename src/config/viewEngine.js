33b7a772bd5ceef61f6e9912f7b2ca03947b2674
Clearlove7 <98686153+alison16smotthie@users.noreply.github.com>
2024-01-01 14:50:14 +0700

    Update viewEngine.js

--- src/config/viewEngine.js
+++ src/config/viewEngine.js
@@ -30,7 +30,7 @@ let configViewEngine = (app, bodyParser, handlebars, SESSION_SECRET, SESSION_ALG
 
     app.use(cors({
         origin: "https://webchat-react-app1.vercel.app",
-        methods: 'GET,POST,PUT,DELETE',
+        methods: 'GET,POST,PUT,DELETE,OPTIONS',
         allowedHeaders: ['Content-Type', 'Authorization', 'X-CSRF-Token'],
         credentials: true,
         optionsSuccessStatus: 204,
