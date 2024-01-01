05495e5ec9be7fc913c4e5c75025952246ef0652
Clearlove7 <98686153+alison16smotthie@users.noreply.github.com>
2024-01-01 15:43:03 +0700

    update

--- src/controller/api/api_AuthController.js
+++ src/controller/api/api_AuthController.js
@@ -42,10 +42,10 @@ class api_AuthController {
 
     account = async (req, res, next) => {
 
-      // res.header('Access-Control-Allow-Origin', process.env.REACT_APP_HOSTNAME);
-      // res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
-      // res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization','X-CSRF-Token');
-      // res.header('Access-Control-Allow-Credentials', 'true');
+      res.header('Access-Control-Allow-Origin', process.env.REACT_APP_HOSTNAME);
+      res.header('Access-Control-Allow-Methods', 'GET, POST, PUT, DELETE');
+      res.header('Access-Control-Allow-Headers', 'Content-Type, Authorization','X-CSRF-Token');
+      res.header('Access-Control-Allow-Credentials', 'true');
 
       return res.status(200).send({
         
